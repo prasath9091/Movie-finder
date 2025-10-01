4fdd987a7a05a5c53b59e23dca1fa650d1b96ca6
prasath9091 <130091501+prasath9091@users.noreply.github.com>
2025-10-01 20:29:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 // export default defineConfig({
 //   base: '/Movie-finder/',
 //   plugins: [react()],
 // })
+// export default defineConfig({
+//   base: "/",
+//   build: {
+//     outDir: "dist"
+//   }
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
 export default defineConfig({
   base: "/",
+  plugins: [react()],
   build: {
     outDir: "dist"
   }
-})
+});
+
 
